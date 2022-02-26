896a09a0aae09336935e4380e816c97720282844
ramimgamal <ramimgamal@gmail.com>
2022-02-26 10:23:26 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes,RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MyNewComponentComponent } from './LIst-component/my-new-component.component';
 import { ListComponentComponent } from './my-component/List-component.component';
 import { NavBarComponentComponent } from './NavBar-component/NavBar-component.component';
 import { HousingService } from './Service/Housing.service';
+import { AddPropertyComponent } from './add-property/add-property.component';
 
+const appRoutes: Routes = [
+  { path: "add-property", component: AddPropertyComponent },
+  { path: "", component: MyNewComponentComponent },
+  //{ path: "contactus", component: ContactUsComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MyNewComponentComponent,
       ListComponentComponent,
-      NavBarComponentComponent
+      NavBarComponentComponent,
+      AddPropertyComponent
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [HousingService],
   bootstrap: [AppComponent],
